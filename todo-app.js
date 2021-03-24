4f7c1a4bedff2cbed8a27bf4e49fc5f29bfbf09c
Jacob Harvey <Jacobharv00@gmail.com>
2021-03-23 18:43:01 -0600

    Add files via upload

---
+++ todo-app.js
@@ -0,0 +1,82 @@
+const todos = getSavedTodos() 
+
+const filters = {
+    searchText: '',
+    hideCompletd: false
+}
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+    todos.push({
+        id: uuidv4(),
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    saveTodos(todos)
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
+
+
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
+    filters.hideCompletd = e.target.checked 
+    renderTodos(todos, filters)
+})
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Saved Notes:
+
+//const paragraphs = document.querySelectorAll('p')
+
+//paragraphs.forEach(function (paragraph) {
+  // if (paragraph.textContent.includes('the')) {
+    //   paragraph.remove()
+   // }
+// })
+
+// Listen for new todo creation
+//document.querySelector('#add-todo').addEventListener('click', function (e) {
+  //  console.log('Add a new todo!')
+    // e.target.textContent = 'Add a new todo!' will change what the button says after it was clicked
+// })
+
+// Listen for todo text change
+// document.querySelector('#new-todo').addEventListener('input', function (e) {
+   // console.log(e.target.value)
+// })
--- todo-functions.js
+++ todo-functions.js
@@ -1,1 +1,86 @@
+// Fetch existing todos from localStorage
+const getSavedTodos = () => {
+    const todosJSON = localStorage.getItem('todos')
+    return todosJSON ? JSON.parse(todosJSON) : []
+}
+
+// Save todos to localStorage
+const saveTodos = (todos) => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Remove a todo by id
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id)
+    
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }    
+}
+
+// Toggle the completed value for a given todo
+const toggleTodo = (id) => {
+    const todo = todos.find((todo) => todo.id === id)
+
+    if (todo) {
+        todo.completed = !todo.completed
+    }
+
+}
+
+// Render application todos based on filters
+const renderTodos = (todos, filters) => {
+    const filteredTodos = todos.filter((todo) => {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompletd || !todo.completed
+        return searchTextMatch && hideCompletedMatch
+    })
+
+    const incompleteTodos = filteredTodos.filter((todo) => !todo.completed) 
+
+    document.querySelector('#todos').innerHTML = '' 
+    document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
+      
+    filteredTodos.forEach((todo) => {
+        document.querySelector('#todos').appendChild(generateTodoDOM(todo))
+    })
+}
+
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement('div')
+    const checkbox = document.createElement('input')
+    const todoText = document.createElement('span')
+    const removeButton = document.createElement('button')
+
+    // Setup todo checkbox
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.checked = todo.completed
+    todoEl.appendChild(checkbox)
+    checkbox.addEventListener('change', () => {
+        toggleTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
+
+    // Setup todo text
+    todoText.textContent = todo.text
+    todoEl.appendChild(todoText)
+
+    // Setup the remove todo button
+    removeButton.textContent = 'x'
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
+
+    return todoEl
+}
+
+const generateSummaryDOM = (incompleteTodos) => {
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    return summary
+}
 
